52b3aaf762eded0ac208903a6432980778a37491
jiangrui <jiangrui@zhowin.com>
2019-01-24 20:17:10 +0800

    init

---
+++ app/Greeter.js
@@ -0,0 +1,10 @@
+//Greeter,js
+import React, { Component } from 'react';
+import config from './config.json';
+import styles from './Greeter.css';
+
+export default class Greeter extends Component {
+    render() {
+        return <div className={styles.root}>Hello, { config.greetText } </div>;
+    }
+}
